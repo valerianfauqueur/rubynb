c5d11544808585aa9b9b5abdcf89b4e2c40a8646
valerianfauqueur <valerian.fauqueur@hetic.net>
2017-12-18 01:42:48 +0100

    reset:config

--- config/puma.rb
+++ config/puma.rb
@@ -14,7 +14,6 @@ port        ENV.fetch("PORT") { 3000 }
 # Specifies the `environment` that Puma will run in.
 #
 environment ENV.fetch("RAILS_ENV") { "development" }
-environment ENV.DISABLE_DATABASE_ENVIRONMENT_CHECK=1
 
 # Specifies the number of `workers` to boot in clustered mode.
 # Workers are forked webserver processes. If using threads and workers together
